f64c7a58a5ccfdc7ca0ff2a973e89a9417edfcfc
Dawodu Bolaji <dawodubolaji189@gmail.com>
2024-02-23 11:22:25 +0100

    initial commit

---
+++ lib/database/models/user.model.ts
@@ -0,0 +1,29 @@
+// clerkId, email, username, photo, firstName, lastName, planId, creditBalance
+
+import { Schema, model, models } from "mongoose";
+
+interface IUser {
+    clerkId: string;
+    firstName: string;
+    lastName: string;
+    username: string;
+    email: string;
+    photo: string;
+    planId?: number;
+    creditBalance?: number;
+}
+
+const UserSchema = new Schema({
+    clerkId: { type: String, required: true, unique: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    username: { type: String, required: true, unique: true  },
+    email: { type: String, required: true, unique: true  },
+    photo: { type: String, required: true },
+    planId: { type: Number, default: 1 },
+    creditBalance: { type: Number, default: 10 },
+})
+
+const User = models?.User || model('User', UserSchema);
+
+export default User;
